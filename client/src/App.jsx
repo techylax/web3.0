984b3de1b8b9d9845530bb171da1a1f8cb496a24
Laxman Poudel <poudell@lafayette.edu>
2023-06-02 09:06:25 -0400

    tailwind config

---
+++ client/src/App.jsx
@@ -0,0 +1,16 @@
+
+const App = ()=> {
+
+  return (
+    
+      <div className = "App">
+      <h1 class="text-3xl font-bold underline">
+         Hello world!
+     </h1>        
+      </div>
+    
+    
+  )
+}
+
+export default App
--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -1,9 +1,8 @@
 /** @type {import('tailwindcss').Config} */
-export default {
-  content: [],
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
   theme: {
     extend: {},
   },
   plugins: [],
 }
-
