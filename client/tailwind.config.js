f1c2874f0dd7acb637d68e285709100c1f0ae7b1
Laxman Poudel <poudell@lafayette.edu>
2023-06-02 09:13:07 -0400

    solved tailwind setup after changing content format

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -1,6 +1,8 @@
-/** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{html,js}"],
+  content: ["./src/**/*.{html,jsx}",
+
+  '*.{html,js}'], //like this
+
   theme: {
     extend: {},
   },
