9d005e94b25393c638e401689cb2f634a9aa0ea3
Laxman Poudel <poudell@lafayette.edu>
2023-06-05 11:29:16 -0400

    woring welcome page with updated tailwingcss

--- client/src/components/Welcome.jsx
+++ client/src/components/Welcome.jsx
@@ -13,58 +13,46 @@ const Input = ({ placeholder, name, type, value, handleChange }) => (
     className="my-2 w-full rounded-sm p-2 outline-none bg-transparent text-white border-none text-sm white-glassmorphism"
   />
 );
-const Welcome = () =>
-{
-  const commonStyles = "min-h-[70px] sm:px-0 px-2 sm:min-w-[120px] flex justify-center items-center border-[0.5px] border-gray-400 text-sm font-light text-white";    
-    
-     const connectWallet = ()=>
-     {
 
-     }
-    
-     const handleSubmit = (e) => {
-      const { addressTo, amount, keyword, message } = formData;
-  
+  const connectWallet = () => {
+    // Connect wallet logic goes here
+  };
 
-    return (
-        <div className = "flex w-full justify-center items-center">
-           <div className = "flex md:flex row flex-col items-start justify-between md:p20 py-12 px-4 ">
-             <div className = "flex flex-1 justify-start flex-col md:mr-10 ">
-                <h1 className="text-3xl sm:text-5xl text-white text-gradient py-1">
-                    Send Crypto <br/> across the world 
+  const handleSubmit = (e) => {
+    const { addressTo, amount, keyword, message } = formData;
+    // Handle submit logic goes here
+  };
 
-                </h1>
-                <p className ="text-left mt-5 text-white font-light md:w-9/12 w-11/12 text-base">Explore the crypto world.Buy and sell cryptocurrencies easily on Krypto.com </p>
-                <button type = "button "
-                  onClick={connectWallet}
-                  className="flex flex-row justify-center items-center my-5 bg-[#2952e3] p-3 rounded-full cursor-pointer hover:bg-[#2546bd]"
-                  >
-                    <p className = "text-white text-base font-semibold">Connect Wallet</p>
+const Welcome = () => {
+
+  const commonStyles = "min-h-[70px] sm:px-0 px-2 sm:min-w-[120px] flex justify-center items-center border-[0.5px] border-gray-400 text-sm font-light text-white";
 
 
-                  </button>
-                  <div className="grid sm:grid-cols-3 grid cols-2 w-full mt-10">
-                   <div className={`rounded-tl-2xl ${commonStyles}`}>
-                        Reliability
-                   </div>
-                   <div className = {commonStyles}>Security</div>
-                   <div className ={`rounded-tr-2xl ${commonStyles}`}> Etherum
+  return (
+    <div className="flex w-full justify-center items-center">
+      <div className="flex md:flex-row flex-col items-start justify-between md:p-20 py-12 px-4">
+        <div className="flex flex-1 justify-start flex-col md:mr-10">
+          <h1 className="text-3xl sm:text-5xl text-white text-gradient py-1">
+            Send Crypto <br /> across the world
+          </h1>
+          <p className="text-left mt-5 text-white font-light md:w-9/12 w-11/12 text-base">Explore the crypto world. Buy and sell cryptocurrencies easily on Krypto.com</p>
+          <button
+            type="button"
+            onClick={connectWallet}
+            className="flex flex-row justify-center items-center my-5 bg-[#2952e3] p-3 rounded-full cursor-pointer hover:bg-[#2546bd]"
+          >
+            <p className="text-white text-base font-semibold">Connect Wallet</p>
+          </button>
+          <div className="grid sm:grid-cols-3 grid-cols-2 w-full mt-10">
+            <div className={`rounded-tl-2xl ${commonStyles}`}>Reliability</div>
+            <div className={commonStyles}>Security</div>
+            <div className={`rounded-tr-2xl ${commonStyles}`}>Etherum</div>
+            <div className={`rounded-bl-2xl ${commonStyles}`}>Web3.0</div>
+            <div className={commonStyles}>Low fees</div>
+            <div className={`rounded-br-2xl ${commonStyles}`}>Blockchain</div>
+          </div>
+        </div>
 
-                   </div>
-                   <div className ={` rounded-bl-2xl ${commonStyles}`}> Web3.0
-
-                   </div>
-                   <div className ={`${commonStyles}`}> Low fees 
-
-</div>
-                  <div className ={` rounded-br-2xl ${commonStyles}`}> Blockchain 
-
-                  </div>
-                  </div>
-
-             </div>
-                  
-                  
         <div className="flex flex-col flex-1 items-center justify-start w-full mf:mt-0 mt-10">
           <div className="p-3 flex justify-end items-start flex-col rounded-xl h-40 sm:w-72 w-full my-5 eth-card .white-glassmorphism ">
             <div className="flex justify-between flex-col w-full h-full">
@@ -75,42 +63,35 @@ const Welcome = () =>
                 <BsInfoCircle fontSize={17} color="#fff" />
               </div>
               <div>
-                <p className="text-white font-light text-sm">
-                 Address
-                </p>
-                <p className="text-white font-semibold text-lg mt-1">
-                  Ethereum
-                </p>
+                <p className="text-white font-light text-sm">Address</p>
+                <p className="text-white font-semibold text-lg mt-1">Ethereum</p>
               </div>
             </div>
           </div>
           <div className="p-5 sm:w-96 w-full flex flex-col justify-start items-center blue-glassmorphism">
-            <Input placeholder="Address To" name="addressTo" type="text" handleChange={handleChange} />
-            <Input placeholder="Amount (ETH)" name="amount" type="number" handleChange={handleChange} />
-            <Input placeholder="Keyword (Gif)" name="keyword" type="text" handleChange={handleChange} />
-            <Input placeholder="Enter Message" name="message" type="text" handleChange={handleChange} />
+            <Input placeholder="Address To" name="addressTo" type="text" handleChange ={()=>{}} />
+            <Input placeholder="Amount (ETH)" name="amount" type="number" handleChange={()=>{}} />
+            <Input placeholder="Keyword (Gif)" name="keyword" type="text" handleChange={()=>{}} />
+            <Input placeholder="Enter Message" name="message" type="text" handleChange={()=>{}} /> 
 
             <div className="h-[1px] w-full bg-gray-400 my-2" />
 
-            {false
-              ? (<Loader />
-              ): (
-                <button
-                  type="button"
-                  onClick={handleSubmit}
-                  className="text-white w-full mt-2 border-[1px] p-2 border-[#3d4f7c] hover:bg-[#3d4f7c] rounded-full cursor-pointer"
-                >
-                  Send now
-                </button>
-              )}
+            {false ? (
+              <Loader />
+             ) : (
+              <button
+                type="button"
+                onClick={handleSubmit}
+                className="text-white w-full mt-2 border-[1px] p-2 border-[#3d4f7c] hover:bg-[#3d4f7c] rounded-full cursor-pointer"
+              >
+                Send now
+              </button>
+            )}
           </div>
         </div>
-
-             </div>
-        </div>
-    );
- 
-
-}
+      </div>
+    </div>
+  );
+};
 
 export default Welcome;
