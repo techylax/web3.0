9b9e01736a247737de1f2d25254b94755a611830
Laxman Poudel <poudell@lafayette.edu>
2023-06-06 08:51:56 -0400

    hardhat config file

--- smart_contract/hardhat.config.js
+++ smart_contract/hardhat.config.js
@@ -1,6 +1,15 @@
-require("@nomicfoundation/hardhat-toolbox");
 
-/** @type import('hardhat/config').HardhatUserConfig */
-module.exports = {
-  solidity: "0.8.0",
-};
+reuire('@nomiclabs/hardhat-waffle');
+
+module.exports =
+{
+    solidity :'0.8.0',
+    networks:
+    {
+        repsten:
+        {
+            url:'',
+            accounts: ['']
+        }
+    }
+}
--- smart_contract/scripts/deploy.js
+++ smart_contract/scripts/deploy.js
@@ -1,32 +1,28 @@
-// We require the Hardhat Runtime Environment explicitly here. This is optional
-// but useful for running the script in a standalone fashion through `node <script>`.
-//
-// You can also run a script with `npx hardhat run <script>`. If you do that, Hardhat
-// will compile your contracts, add the Hardhat Runtime Environment's members to the
-// global scope, and execute the script.
-const hre = require("hardhat");
 
-async function main() {
-  const currentTimestampInSeconds = Math.round(Date.now() / 1000);
-  const unlockTime = currentTimestampInSeconds + 60;
 
-  const lockedAmount = hre.ethers.utils.parseEther("0.001");
+const main = async () => {
 
-  const Lock = await hre.ethers.getContractFactory("Lock");
-  const lock = await Lock.deploy(unlockTime, { value: lockedAmount });
 
-  await lock.deployed();
+  const Transactions = await hre.ethers.getContractFactory("Transactions");
+  const transactions = await Transactions.deploy();
 
-  console.log(
-    `Lock with ${ethers.utils.formatEther(
-      lockedAmount
-    )}ETH and unlock timestamp ${unlockTime} deployed to ${lock.address}`
-  );
+  await Transactions.deployed();
+
+  console.log("Transactions deployed to:", transactions.address);
 }
 
-// We recommend this pattern to be able to use async/await everywhere
-// and properly handle errors.
-main().catch((error) => {
+
+const runMain = async ()=>
+{
+ try{
+  await main();
+  process.exit(0);
+ }
+ catch(error)
+ {
   console.error(error);
-  process.exitCode = 1;
-});
+  process.exit(1);
+ }
+}
+
+runMain();
