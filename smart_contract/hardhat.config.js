04a92360efca03ce3bb22887d903d01c21f5e8aa
Laxman Poudel <coderlaxman@gmail.com>
2023-06-08 19:35:47 -0400

    config update

--- smart_contract/hardhat.config.js
+++ smart_contract/hardhat.config.js
@@ -8,7 +8,7 @@ module.exports =
     {
         goerli:
         {
-            url:'https://eth-mainnet.g.alchemy.com/v2/gsy807EgqGAkq7ydZ1pLf50Xhlk6vtDB',
+            url:'https://eth-mainnet.g.alchemy.com/v2/SggqhxhNZyj7BMjtFQKMz8eqyN4ctKgP',
             accounts: ['9155028d14d6aea20aa599042e2ff5c2d96d16f7b63523f958659ab4a62d4b6f']
         }
     }
