f79afc3917452d3a1271541c0afafd6e30e5dfb7
Laxman Poudel <poudell@lafayette.edu>
2023-06-09 10:22:33 -0400

    Transaction JSX update

--- client/src/context/TranscationContext.jsx
+++ client/src/context/TranscationContext.jsx
@@ -6,4 +6,25 @@ export const TransactionContext = React.createContext();
 
 const { ethereum } = window;
 
- 
+const getEthereumContract = () => {
+    const provider = new ethers.providers.Web3Provider(ethereum);
+    const signer = provider.getSigner();
+    const transactionsContract = new ethers.Contract(contractAddress, contractABI, signer);
+
+    console.log(
+        {
+            provider,
+            signer,
+            transactionsContract
+        }
+
+    );
+}
+
+export const TransactionsProvider = ({ children }) => {
+    return (
+        <TransactionContext.Provider value = {{}}>
+            {children}
+        </TransactionContext.Provider>
+    );
+}
