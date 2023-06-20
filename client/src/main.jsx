0ce720b74b1c54934057191fb8018e474a506a23
Laxman Poudel <poudell@lafayette.edu>
2023-06-20 16:28:17 -0400

    updates index and main jsx

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,13 +1,13 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
-import { TransactionProvider } from './context/TranscationContext'
+import React from "react";
+import ReactDOM from "react-dom";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-<TransactionProvider>
-  <React.StrictMode>
+import App from "./App";
+import { TransactionsProvider } from "./context/TransactionContext";
+import "./index.css";
+
+ReactDOM.render(
+  <TransactionsProvider>
     <App />
-  </React.StrictMode>,
-  </TransactionProvider>
-)
+  </TransactionsProvider>,
+  document.getElementById("root"),
+);
